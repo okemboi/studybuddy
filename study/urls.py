9d6a15990452a124d8b788707d21e6942b7570cf
kkemboi <79087047+okemboi@users.noreply.github.com>
2023-11-02 14:00:09 +0300

    update base

--- study/urls.py
+++ study/urls.py
@@ -1,7 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+def home(requests):
+    return HttpResponse('Home Page')
 
+def home ()
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
 ]
